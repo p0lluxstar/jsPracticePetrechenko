3ad70d4a283e56fcc02f3d75e83a3ba5da61d7ac
p0lluxstar <web-registra@yandex.ru>
2022-10-27 20:55:03 +0300

    w-271022-20:50

---
+++ project1/src/js/modules/modals.js
@@ -0,0 +1,9 @@
+const modals = () => {
+    function bindModal(trigger, modal, close) {
+        trigger.addEventListener('click', () => {
+            
+        }) 
+    }
+};
+
+export default modals;
